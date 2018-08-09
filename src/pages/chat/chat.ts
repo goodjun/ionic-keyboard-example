d39c2194d323cff0fd9bd12d46eb469bbaf41bb3
goodjun <goodjun@163.com>
2018-08-08 19:15:15 -0700

    keybord bugfix

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Keyboard } from '@ionic-native/keyboard';
 
 @Component({
-  selector: 'page-chat',
-  templateUrl: 'chat.html'
+    selector: 'page-chat',
+    templateUrl: 'chat.html',
+    providers: [Keyboard]
 })
 export class ChatPage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(
+        public navCtrl: NavController,
+        public platform: Platform,
+        public keyboard: Keyboard,
+    ) {
 
-  }
+        if (platform.is('ios')) {
+            let appEl = <HTMLElement>(document.getElementsByTagName('ION-APP')[0]);
+            let appElHeight = appEl.clientHeight;
+
+            this.keyboard.disableScroll(true);
+
+            window.addEventListener('native.keyboardshow', (e) => {
+                appEl.style.height = (appElHeight - (<any>e).keyboardHeight) + 'px';
+            });
+
+            window.addEventListener('native.keyboardhide', () => {
+                appEl.style.height = '100%';
+            });
+        }
+
+    }
+
+    ionViewDidload() {
+        this.keyboard.disableScroll(true);
+    }
 
 }
